5baf935c5c64bb68bbc83d7054df0980d482387d
CHANDHRU R <44549194+chan2412@users.noreply.github.com>
2020-04-23 16:52:53 +0530

    Updates

---
+++ precache-manifest.dcc7e1c3e64c2e5003ec051c864cec18.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "241067ee71bd68f14adfba79f7d34cf7",
+    "url": "/todolist/index.html"
+  },
+  {
+    "revision": "a7e98ffa0bcb25ef36db",
+    "url": "/todolist/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "3a5921479b16b2a811bb",
+    "url": "/todolist/static/js/2.b6bbe25a.chunk.js"
+  },
+  {
+    "revision": "3adc01bea76e7956dc3633ee898f6936",
+    "url": "/todolist/static/js/2.b6bbe25a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a7e98ffa0bcb25ef36db",
+    "url": "/todolist/static/js/main.cf498ccd.chunk.js"
+  },
+  {
+    "revision": "7f6cc449a87a2498d1f0",
+    "url": "/todolist/static/js/runtime-main.5bd63c2e.js"
+  }
+]);
